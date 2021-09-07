801e60e7cf8d8eb51e44365c97d4dcb92f861104
Damien Menanteau <damien.menanteau@gmail.com>
2021-09-07 17:55:30 +0200

    Add "using namespace" to simplify function calls

--- cpp/test/MarsRoverTest.cpp
+++ cpp/test/MarsRoverTest.cpp
@@ -3,9 +3,11 @@
 
 #include <gtest/gtest.h>
 
+using namespace com::murex::kata::mars_rover;
+
 TEST(MarsRover, DISABLED_acceptance_test)
 {
-	kata::mars_rover::MarsRover rover(10, 10, 'N');
+	MarsRover rover(10, 10, 'N');
 	rover.receiveCommands("LFFLBRRBBBRFFFLL");
 	EXPECT_EQ("11 8 W", rover.sendPosition());
 }
