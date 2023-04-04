8c98acd6722888f43d200c0ff5f1c1f3d9fd4ffe
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-04 18:27:42 +0200

    Change python test framework from unittest to pytest

--- python/tests/mars_rover_test.py
+++ python/tests/mars_rover_test.py
@@ -1,16 +1,12 @@
-import unittest
+import pytest
 
 from mars_rover.mars_rover import MarsRover
 
 
-class MarsRoverTest(unittest.TestCase):
+class TestMarsRover:
 
-    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
-    def test_acceptance_test(self):
+    @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
+    def test_acceptance_test(self) -> None:
         rover = MarsRover(10, 10, 'N')
         rover.receive_commands('LFFLBRRBBBRFFFLL')
-        self.assertEqual('11 8 W', rover.send_position())
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert ('11 8 W' == rover.send_position())
